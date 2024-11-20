3027a5f6ffa0d89a6784d69c02ca8bd424ad33d5
alger <algerkc@qq.com>
2024-11-20 22:44:17 +0800

    ✨ feat: 完善mac打包规则 修复 icon显示问题

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function createWindow() {
   } else {
     win.loadURL(`file://${__dirname}/dist/index.html`);
   }
-  const image = nativeImage.createFromPath(path.join(__dirname, 'public/icon.png'));
+  const image = nativeImage.createFromPath(path.join(__dirname, 'public/icon_16x16.png')).resize({ width: 16, height: 16 });
   const tray = new Tray(image);
 
   // 创建一个上下文菜单
