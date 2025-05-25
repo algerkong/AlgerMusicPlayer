7685ad39393709545876c9b02598c0ce808e42a8
alger <algerkc@qq.com>
2025-05-25 11:35:16 +0800

    feat: 在配置中添加 publicDir 选项以指定资源目录

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
         resolvers: [NaiveUiResolver()]
       })
     ],
+    publicDir: resolve('resources'),
     server: {
       proxy: {
         // with options
